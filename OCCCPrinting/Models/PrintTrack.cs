55c33f0b4cca2aabe4139c853c8512bdf72160f1
Soufiane Belmir <belmir.soufiane@gmail.com>
2018-09-05 05:02:47 -0500

    SettingAdded

---
+++ OCCCPrinting/Models/PrintTrack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCCCPrinting.Models
+{
+    public class PrintTrack
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
